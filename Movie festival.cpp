cbe4989faca4f6c8541642905964e62832a6fb97
UKS28 <90481142+UKS28@users.noreply.github.com>
2023-12-20 17:02:04 +0530

    Add files via upload

--- Movie festival.cpp
+++ Movie festival.cpp
@@ -2,6 +2,12 @@
 using namespace std;
 #define rep(k,z) for(int i=k;i<z;i++)
 
+bool cust(pair<int,int> &a,pair<int,int> &b)
+{
+	if(a.second<b.second) return true;
+	return false;
+}
+
 int main(){
     int n;
 	cin>>n;
@@ -12,19 +18,18 @@ int main(){
 	   int a,b;
 	   cin>>a>>b;
        vp.push_back({a,b});
-	   
 	}
     
-	sort(vp.begin(),vp.end());
+	sort(vp.begin(),vp.end(),cust);
 
 	int ans=1;
 	int last=vp[0].second;
 	rep(1,n)
 	{
-		if(last<=vp[i].second)
-		   ans++,last=vp[i].second;
+		// cout<<last<<"l";
+		if(last<=vp[i].first)
+		   {ans++;last=vp[i].second;}
 	}
 	cout<<ans<<endl;
 	return 0;
 }
-
